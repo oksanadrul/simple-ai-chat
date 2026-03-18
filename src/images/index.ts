8031e920ec31003515b38748e727652060f69a9d
Oxana Drul <drul.oksana@gmail.com>
2026-03-18 16:40:32 +0200

    [OD] Editing images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,8 +1,14 @@
 import OpenAI from "openai";
 import * as fs from "fs";
+import { toFile } from "openai";
 
 const openai = new OpenAI();
-const MODEL = "dall-e-3";
+const MODEL = "dall-e-2";
+
+async function loadPngUpload(filePath: string, uploadName: string) {
+  const data = await fs.promises.readFile(filePath);
+  return toFile(data, uploadName, { type: "image/png" });
+}
 
 async function createImage() {
   const response = await openai.images.generate({
@@ -49,8 +55,45 @@ async function createAdvancedImage() {
   const imageData = response.data?.[0].b64_json;
   if (imageData) {
     const buffer = Buffer.from(imageData, "base64");
-    fs.writeFileSync("advanced_image.png", buffer);
+    fs.writeFileSync("city.png", buffer);
   }
 }
 
-createAdvancedImage();
+async function createImageVariation() {
+  const sourceImage = await loadPngUpload("city.png", "city.png");
+
+  const response = await openai.images.createVariation({
+    model: MODEL,
+    image: sourceImage,
+    response_format: "b64_json",
+    n: 1,
+    size: "1024x1024",
+  });
+
+  const imageData = response.data?.[0].b64_json;
+  if (imageData) {
+    const buffer = Buffer.from(imageData, "base64");
+    fs.writeFileSync("city_variation.png", buffer);
+  }
+}
+
+async function editImage() {
+  const sourceImage = await loadPngUpload("city.png", "city.png");
+  const maskImage = await loadPngUpload("cityMask.png", "cityMask.png");
+
+  const response = await openai.images.edit({
+    model: MODEL,
+    image: sourceImage,
+    mask: maskImage,
+    prompt: "Add flying cars in the sky, futuristic style",
+    response_format: "b64_json",
+    n: 1,
+  });
+  const imageData = response.data?.[0].b64_json;
+  if (imageData) {
+    const buffer = Buffer.from(imageData, "base64");
+    fs.writeFileSync("city_edited.png", buffer);
+  }
+}
+
+editImage();
