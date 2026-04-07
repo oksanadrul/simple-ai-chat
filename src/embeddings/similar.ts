53ea7dac377e62ccceb12bdc259a6847a8b2b475
Oxana Drul <drul.oksana@gmail.com>
2026-04-07 11:02:19 +0300

    [OD] Embedding, similarity and embeddings videoMovie search

--- src/embeddings/similar.ts
+++ src/embeddings/similar.ts
@@ -1,48 +1,61 @@
 import { DataWithEmbeddings, generateEmbeddings, loadJSONData } from "./main";
 
-function dotProduct(a: number[], b: number[]) {
-    return a.map((value, index) => value * b[index]).reduce((a, b) => a + b, 0);
+export function dotProduct(a: number[], b: number[]) {
+  return a.map((value, index) => value * b[index]).reduce((a, b) => a + b, 0);
 }
 
 function cosineSimilarity(a: number[], b: number[]) {
-    const product = dotProduct(a, b);
-    const aMagnitude = Math.sqrt(a.map(value => value * value).reduce((a, b) => a + b, 0));
-    const bMagnitude = Math.sqrt(b.map(value => value * value).reduce((a, b) => a + b, 0));
-    return product / (aMagnitude * bMagnitude);
+  const product = dotProduct(a, b);
+  const aMagnitude = Math.sqrt(
+    a.map((value) => value * value).reduce((a, b) => a + b, 0),
+  );
+  const bMagnitude = Math.sqrt(
+    b.map((value) => value * value).reduce((a, b) => a + b, 0),
+  );
+  return product / (aMagnitude * bMagnitude);
 }
 
 async function main() {
-    const dataWithEmbeddings = loadJSONData<DataWithEmbeddings[]>('dataWithEmbeddings.json')
+  const dataWithEmbeddings = loadJSONData<DataWithEmbeddings[]>(
+    "dataWithEmbeddings.json",
+  );
 
-    // Example input to compare with the embeddings in the data
-    const input = 'animal';
+  // Example input to compare with the embeddings in the data
+  const input = "animal";
 
-    const inputEmbedding = await generateEmbeddings(input);
+  const inputEmbedding = await generateEmbeddings(input);
 
-    const similarities: {
-        input: string,
-        similarity: number
-        dotProductValue: number
-    }[] = [];
+  const similarities: {
+    input: string;
+    similarity: number;
+    dotProductValue: number;
+  }[] = [];
 
-    for (const entry of dataWithEmbeddings) {
-        const similarity = cosineSimilarity(
-            entry.embedding,
-            inputEmbedding.data[0].embedding
-        )
-        const dotProductValue = dotProduct(entry.embedding, inputEmbedding.data[0].embedding);
-        similarities.push({
-            input: entry.input,
-            similarity,
-            dotProductValue
-        })
-    }
+  for (const entry of dataWithEmbeddings) {
+    const similarity = cosineSimilarity(
+      entry.embedding,
+      inputEmbedding.data[0].embedding,
+    );
+    const dotProductValue = dotProduct(
+      entry.embedding,
+      inputEmbedding.data[0].embedding,
+    );
+    similarities.push({
+      input: entry.input,
+      similarity,
+      dotProductValue,
+    });
+  }
 
-    console.log(`Similarity of ${input} with:`)
-    const sortedSimilarities = similarities.sort((a, b) => b.similarity - a.similarity);
-    sortedSimilarities.forEach(similarity => {
-        console.log(`${similarity.input}: ${similarity.similarity} (dot product: ${similarity.dotProductValue})`);
-    })
+  console.log(`Similarity of ${input} with:`);
+  const sortedSimilarities = similarities.sort(
+    (a, b) => b.similarity - a.similarity,
+  );
+  sortedSimilarities.forEach((similarity) => {
+    console.log(
+      `${similarity.input}: ${similarity.similarity} (dot product: ${similarity.dotProductValue})`,
+    );
+  });
 }
 
 main();
